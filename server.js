3d5f5c60ce307762e77074be713fae0c2113bfbb
Craig Griffith <craig429@mac.com>
2017-08-23 18:33:33 +0000

    Added tabstops to vim

--- server.js
+++ server.js
@@ -10,18 +10,20 @@ app.set('view engine', 'ejs');
 
 // index page 
 app.get('/', function(req, res) {
-		var drinks = [
-		        { name: 'Bloody Mary', drunkness: 3 },
-		        { name: 'Martini', drunkness: 5 },
-		        { name: 'Scotch', drunkness: 10 }
-		    ];
-		    var tagline = "Blah blah blah";
+  var drinks = [
+    { name: 'Bloody Mary', drunkness: 3 },
+    { name: 'Martini', drunkness: 5 },
+    { name: 'Scotch', drunkness: 10 },
+    { name: 'LSD', drunkness: 55 },
+  ];
+  
+  var tagline = "Blah blah blah";
 
 
-    res.render('pages/index', {
-        drinks: drinks,
-        tagline: tagline
-    });
+  res.render('pages/index', {
+    drinks: drinks,
+    tagline: tagline
+  });
 });
 
 // about page 
